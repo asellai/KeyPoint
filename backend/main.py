3bd0b0cdf5e7296e7eed65e2f6217cc0cb3a837f
asellai <asel.alibekkyzy@alatoo.edu.kg>
2026-05-24 21:05:34 +0600

    security: fix CORS with correct frontend URL

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
     CORSMiddleware,
 allow_origins=[
     "http://localhost:3000",
-    "https://feisty-elegance.up.railway.app",
+    "https://feisty-elegance-production-26c7.up.railway.app",
     os.getenv("FRONTEND_URL", "http://localhost:3000")
 ],
     allow_credentials=True,
