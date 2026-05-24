21e392da0f86d29200305f794828d908ffdf7735
asellai <asel.alibekkyzy@alatoo.edu.kg>
2026-05-24 21:15:19 +0600

    fix: fix syntax error in auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -13,7 +13,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str):
-    return pwd_context.hash(password[:72]d)
+    return pwd_context.hash(password[:72])
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password[:72], hashed_password)
