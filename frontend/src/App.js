b64c42a2b61f10155afd9920f66384790c6fad26
asellai <asel.alibekkyzy@alatoo.edu.kg>
2026-05-24 18:11:36 +0600

    security: add protected routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,39 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
 import Auth from "./pages/Auth";
+
+
+function PrivateRoute({ children }) {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/" />;
+}
+
+
+function PublicRoute({ children }) {
+  const token = localStorage.getItem("token");
+  return token ? <Navigate to="/home" /> : children;
+}
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/" element={
+          <PublicRoute>
+            <Auth />
+          </PublicRoute>
+        } />
+        <Route path="/home" element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        } />
+        <Route path="/quiz" element={
+          <PrivateRoute>
+            <Quiz />
+          </PrivateRoute>
+        } />
       </Routes>
     </div>
   );
